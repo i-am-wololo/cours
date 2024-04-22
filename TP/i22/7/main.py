6201ac099fbb44af152e1cdee2a7a0522e3b4828
i-am-wololo <35537849+i-am-wololo@users.noreply.github.com>
2024-04-22 10:42:54 +0200

    Update main.py

--- TP/i22/7/main.py
+++ TP/i22/7/main.py
@@ -63,12 +63,12 @@ def is_in(mem_asso,  mot):
     check = []
     HorM = ("MISS", "HIT")
     isin = mot in mem_asso
-    index = 0
+    index = None
     for i in range(len(mem_asso)):
         check.append(mem_asso[i]==mot)
         if mem_asso[i] == mot:
             index = i
-    return (isin, check, index)
+    return (HorM[isin], check, index)
 
 def get_value(ram: dict, number: int)->dict:
     memcase = ram[number]
@@ -76,20 +76,50 @@ def get_value(ram: dict, number: int)->dict:
         memcase['data'] == None
     return memcase
 
-def in_cache(mem_asso: list, mem_classique: dict, adresse: int)->tuple:
+
+def in_cache(mem_asso: list[int], mem_classique: dict, adresse: int)->tuple:
+    word = is_in(mem_asso, adresse)[0]
+    miss = ("MISS", None)
+    if word == "HIT":
+        tag = get_value(mem_classique, word)
+        if tag['ok'] == True:
+            return ('HIT', int(tag[data], 16))
+        return miss
+    return miss
+       
+def in_cache_direct_mapped(mem_direct_mapped: list[dict], adresse: int)->tuple:
+    trueaddress = adresse % len(mem_direct_mapped)
+    if mem_direct_mapped[trueaddress]['ok'] == False:
+        return ("MISS", None)
+    else: 
+        return ("HIT", mem_direct_mapped[trueaddress])
+
+def in_cache_direct_mapped_fixed(ram: list[dict], address: int)->tuple:
+        tag = address//len(ram)
+        trueaddress = address%len(ram)
+        if mem_direct_mapped[trueaddress]['tag'] != tag or mem_direct_mapped[trueaddress]['ok'] == False:
+            return ("MISS", None)
+        else: 
+            return ("HIT", mem_direct_mapped[trueaddress]['data'])
+
+def replace_random(mem_asso, mem)->int:
     pass
-
-
 
 M = [{'ok': True, 'data': 0x44}, {'ok': False, 'data': 0xFF},
          {'ok': True, 'data': 0x22}, {'ok': True, 'data': 0x99}]
 
-print(get_value(M, 3))
-
+mem_direct_mapped = [{'ok': True, 'data': 0x00, 'tag': 0x0},
+                         {'ok': False, 'data': 0xFF, 'tag': 0x1},
+                         {'ok': True, 'data': 0x22, 'tag': 0x0},
+                         {'ok': True, 'data': 0x77, 'tag': 0x1}]
 def tests():
     assert init_ram_list(16) == [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], "something went wrong"
     assert fill_ram_random(init_ram_list(16)) != [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     assert get_value_list([0, 0, 0, 1], 3) == 1
     assert init_ram_dict(2) == {"taille":2}
     assert is_in([4, 1, 2, 0], 3) == ('MISS', [False, False, False, False], None)
+    assert in_cache_direct_mapped_fixed(mem_direct_mapped, 0) == ('HIT', 0)
+    assert in_cache_direct_mapped_fixed(mem_direct_mapped, 7) == ('HIT', 119)
+    assert in_cache_direct_mapped_fixed(mem_direct_mapped, 1) == ('MISS', None)
 
+tests()
